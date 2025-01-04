f24f11040d10f68e0273258659c5ec60721b64b9
gitcoder-23 <prabhatece21@gmail.com>
2025-01-04 20:14:59 +0530

    all

---
+++ lib/pages/image_text_invoice.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ImageTextInvoice extends StatelessWidget {
+  const ImageTextInvoice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
